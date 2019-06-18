dc54bd5b001d07b8747383e2edcd4f3c3ac58cfa
YerrabelliHarshini <50914028+YerrabelliHarshini@users.noreply.github.com>
2019-06-18 10:22:43 +0530

    Test code pushed to repository.

---
+++ Printing  stars in vertical fashion/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main
+{
+  public static void main(String args[])
+  {
+   //Type your code here
+    Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+    for(int fac_count=1;fac_count<=n;fac_count++)
+    {
+      System.out.println("*");
+    }
+  }
+}
